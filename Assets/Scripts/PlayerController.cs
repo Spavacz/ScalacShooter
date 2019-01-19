5396066f2fe528b2eca3c9b69cd85cec4bdaf4f5
Patryk Grandt <spav@spav.com.pl>
2019-01-19 14:57:08 +0100

    player wall collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,50 @@
 ﻿using UnityEngine;
-
 public class PlayerController : MonoBehaviour {
 	public float speed = 5f;
+	public LayerMask unwalkableLayer;
 	private const string INPUT_HORIZONTAL = "Horizontal Player ";
 	private const string INPUT_VERTICAL = "Vertical Player ";
+	private float radius = 0.5f;
+
+	
+	private Rigidbody2D rig;
 
 	private string InputHorizontal => INPUT_HORIZONTAL + (playerNumber + 1);
 	private string InputVertical => INPUT_VERTICAL + (playerNumber + 1);
 
 	private int playerNumber;
 
+	private void Awake() {
+		rig = GetComponent<Rigidbody2D>();
+	}
+
 	private void Update() {
 		var inputVector = new Vector3(Input.GetAxis(InputHorizontal), 0, Input.GetAxis(InputVertical));
 		var moveVector = inputVector * speed * Time.deltaTime;
-		transform.Translate(moveVector);
+		
+		//transform.Translate(moveVector);
+
+		//Debug.Log(move.magnitude);
+		var hits = Physics.RaycastAll(transform.position, moveVector, radius + moveVector.magnitude, unwalkableLayer);
+		if (hits.Length > 0) {
+			foreach (var hit in hits) {
+				if (hit.transform == transform) {
+					continue;
+				}
+				if (hit.transform.CompareTag("Wall") || hit.transform.CompareTag("Player")) {
+					Debug.Log("X");
+					var newVector = moveVector.normalized * (hit.distance - radius);
+					if (newVector.magnitude < moveVector.magnitude) {
+						moveVector = newVector;
+					}
+				}
+			}
+		}
+//		if (Physics.RaycastAll(transform.position, moveVector, out var hit, radius + moveVector.magnitude, unwalkableLayer)) {
+//			moveVector = moveVector.normalized * (hit.distance - radius);
+//		}
+
+		transform.position += moveVector;
 	}
 
 	public void SetPlayer(int playerNumber) {
