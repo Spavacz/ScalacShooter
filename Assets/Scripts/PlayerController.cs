e53edfdce5ad78457572be39e5d79502a6d149b7
Patryk Grandt <spav@spav.com.pl>
2019-01-19 12:12:12 +0100

    basic player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 5f;
+	public string inputHorizontal = "Horizontal";
+	public string inputVertical = "Vertical";
+
+	private void Update() {
+		var inputVector = new Vector3(Input.GetAxis(inputHorizontal), 0, Input.GetAxis(inputVertical));
+		var moveVector = inputVector * speed * Time.deltaTime;
+		transform.Translate(moveVector);
+	}
+}
