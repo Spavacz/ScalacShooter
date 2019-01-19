c58f934a9503e4c2084b39bd76754fd1f9637b55
serafinmaciej <serafin.maciej@wp.pl>
2019-01-19 16:27:48 +0100

    Added new player prefab

---
+++ Assets/PlayerView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerView : MonoBehaviour {
+    public SpriteRenderer playerRenderer;
+    public Image hpImage;
+
+    public void Init(Sprite sprite) {
+        playerRenderer.sprite = sprite;
+    }
+
+    public void SetHp(float hpPercent) {
+        hpImage.fillAmount = hpPercent;
+    }
+}
