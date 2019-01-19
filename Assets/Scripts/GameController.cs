4074e26a3b46ff15b77427920f3986ab21138d40
Patryk Grandt <spav@spav.com.pl>
2019-01-19 13:22:25 +0100

    4 players

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public Transform playersContainer;
+	public GameObject playerPrefab;
+
+	public int numberOfPlayers = 4;
+	public List<Vector3> spawnPoints;
+
+	private void Start() {
+		for (var i = 0; i < numberOfPlayers; i++) {
+			SpawnPlayer(i);
+		}
+	}
+
+	private void SpawnPlayer(int playerNumber) {
+		var player = Instantiate(playerPrefab, playersContainer).GetComponent<PlayerController>();
+		player.transform.localPosition = spawnPoints[playerNumber % spawnPoints.Count];
+		player.SetPlayer(playerNumber);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,21 @@
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 5f;
-	public string inputHorizontal = "Horizontal";
-	public string inputVertical = "Vertical";
+	private const string INPUT_HORIZONTAL = "Horizontal Player ";
+	private const string INPUT_VERTICAL = "Vertical Player ";
+
+	private string InputHorizontal => INPUT_HORIZONTAL + (playerNumber + 1);
+	private string InputVertical => INPUT_VERTICAL + (playerNumber + 1);
+
+	private int playerNumber;
 
 	private void Update() {
-		var inputVector = new Vector3(Input.GetAxis(inputHorizontal), 0, Input.GetAxis(inputVertical));
+		var inputVector = new Vector3(Input.GetAxis(InputHorizontal), 0, Input.GetAxis(InputVertical));
 		var moveVector = inputVector * speed * Time.deltaTime;
 		transform.Translate(moveVector);
+	}
+
+	public void SetPlayer(int playerNumber) {
+		this.playerNumber = playerNumber;
 	}
 }
